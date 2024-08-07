a13d7b868ccf1a261a69975bdd8a812bb86c82e7
parisosuch-dev <parisosuch.dev@gmail.com>
2024-08-06 17:43:08 -0700

    init app portion and create getTracks() function

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const SPOTIFY_API_ROOT_URL = "https://api.spotify.com/v1";
 
@@ -20,4 +20,23 @@ export async function getPlaylists(accessToken: string) {
   });
 
   return response.data;
+}
+
+export async function getTracks(accessToken: string) {
+  console.log(accessToken);
+  try {
+    const response = await axios.get(SPOTIFY_API_ROOT_URL + "/me/tracks", {
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    });
+    return response.data;
+  } catch (err: any | AxiosError) {
+    if (axios.isAxiosError(err)) {
+      console.error(err.message);
+      console.error(err.response);
+    } else {
+      console.error(err);
+    }
+  }
 }
