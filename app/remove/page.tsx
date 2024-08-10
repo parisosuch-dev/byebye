dce41db4ec18908bd30fa0f9fe7ef5de46db4917
parisosuch-dev <parisosuch.dev@gmail.com>
2024-08-10 11:48:45 -0700

    almost there

--- app/remove/page.tsx
+++ app/remove/page.tsx
@@ -5,10 +5,11 @@ import { AuroraBackground } from "@/components/ui/aurora-background";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Artist, getArtist, removeArtist } from "@/lib/spotify";
+import { Artist, getArtist, removeArtist, Track } from "@/lib/spotify";
 import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { ChangeEvent, useEffect, useState } from "react";
+import { BarLoader } from "react-spinners";
 
 export default function Remove() {
   const router = useRouter();
@@ -17,6 +18,8 @@ export default function Remove() {
 
   const [artist, setArtist] = useState<Artist>();
   const [confirmation, setConfirmation] = useState("");
+  const [removing, setRemoving] = useState(false);
+  const [tracks, setTracks] = useState<Track[]>([]);
 
   const { data: session } = useSession();
 
@@ -48,40 +51,54 @@ export default function Remove() {
 
   const handleButton = () => {
     if (session && artistID) {
-      removeArtist(session.accessToken!, artistID);
+      setRemoving(true);
+      removeArtist(session.accessToken!, artistID).then((res) => {
+        setTracks(res);
+        setRemoving(false);
+      });
     }
   };
 
   return (
     <AuroraBackground className="w-full min-h-screen flex flex-col items-center justify-center p-4 sm:p-0">
-      <Card className="min-h-[300px] relative flex flex-col justify-between p-4 sm:p-8">
-        <div>
-          <h1 className="text-lg sm:text-2xl font-bold">Are you sure?</h1>
-          <p className="text-sm sm:text-base font-light">
-            You are about to remove {artist.name} from all of your liked songs.
-          </p>
-        </div>
-        <div className="space-y-1 sm:space-y-2">
-          <p className="text-xs sm:text-sm">
-            Type <span className="font-bold">{artist.name}</span> to confirm
-          </p>
-          <Input placeholder="artist name..." onChange={handleInput} />
-        </div>
-        <div className="w-full flex justify-end space-x-2">
-          <Button variant="outline" onClick={() => router.back()}>
-            Cancel
-          </Button>
-          <Button
-            disabled={
-              confirmation.toLowerCase() !== artist.name.toLocaleLowerCase()
-            }
-            className="bg-spotify-green"
-            onClick={handleButton}
-          >
-            Confirm
-          </Button>
-        </div>
-      </Card>
+      {removing ? (
+        <Card className="min-h-[300px] relative flex flex-col justify-center items-center space-y-8 p-4 sm:p-8">
+          <h1 className="text-lg sm:text-2xl font-bold">
+            Removing {artist.name} from your Liked Songs...
+          </h1>
+          <BarLoader color="#1DB954" height={4} width={350} />
+        </Card>
+      ) : (
+        <Card className="min-h-[300px] relative flex flex-col justify-between p-4 sm:p-8">
+          <div>
+            <h1 className="text-lg sm:text-2xl font-bold">Are you sure?</h1>
+            <p className="text-sm sm:text-base font-light">
+              You are about to remove {artist.name} from all of your liked
+              songs.
+            </p>
+          </div>
+          <div className="space-y-1 sm:space-y-2">
+            <p className="text-xs sm:text-sm">
+              Type <span className="font-bold">{artist.name}</span> to confirm
+            </p>
+            <Input placeholder="artist name..." onChange={handleInput} />
+          </div>
+          <div className="w-full flex justify-end space-x-2">
+            <Button variant="outline" onClick={() => router.back()}>
+              Cancel
+            </Button>
+            <Button
+              disabled={
+                confirmation.toLowerCase() !== artist.name.toLocaleLowerCase()
+              }
+              className="bg-spotify-green"
+              onClick={handleButton}
+            >
+              Confirm
+            </Button>
+          </div>
+        </Card>
+      )}
     </AuroraBackground>
   );
 }
